c97f1ab1f082dcaf07cf2525668887f1f3c938f0
Atipat-CMU <atipat_d@cmu.ac.th>
2022-09-22 15:03:10 +0700

    _hotfix_ const variable in room.js

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,8 +2,8 @@ import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
   const rooms = readDB();
-  rooms = data.map((room) => {
+  const result = data.map((room) => {
     return { roomId: room.roomId, roomName: room.roomName };
   });
-  return res.json({ ok: true, rooms });
+  return res.json({ ok: true, rooms: result });
 }
