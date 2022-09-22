2ddc6203bb8357f56458009af52630556e2b28e1
Atipat-CMU <atipat_d@cmu.ac.th>
2022-09-22 15:05:56 +0700

    _hotfix_ variable name in room.js

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,7 +2,7 @@ import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
   const rooms = readDB();
-  const result = data.map((room) => {
+  const result = rooms.map((room) => {
     return { roomId: room.roomId, roomName: room.roomName };
   });
   return res.json({ ok: true, rooms: result });
